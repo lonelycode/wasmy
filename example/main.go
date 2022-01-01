ddf785b07171fa3ed32c79fab20b746b56933cdf
Martin Buhr <martin@tyk.io>
2022-01-01 19:35:17 +0000

    .

--- example/main.go
+++ example/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/lonelycode/wasmy/runner"
 )
@@ -15,18 +16,22 @@ func main() {
 	}
 
 	// TODO: make this use application arguments
+	start := time.Now()
 	err := r.WarmUp("/home/vmuser/wasmy/wasm-tests/managedv2.wasm", "myExport")
 	if err != nil {
 		panic(err)
 	}
+	elapsed := time.Since(start)
+	fmt.Printf("wasm load took %s\n", elapsed)
 
 	// TODO: This breaks when there are no args, handle 0 len args
+	start = time.Now()
 	out, err := r.Run("myExport", "martin")
-
 	if err != nil {
 		panic(err)
 	}
-
+	elapsed = time.Since(start)
+	fmt.Printf("wasm run took %s\n", elapsed)
 	fmt.Printf("function output (from runner): %v \n", out)
 
 }
